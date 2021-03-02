67916ebb34432010dd858f109dd12b55942f77d8
Ayrton Souza <ayrtonsouza@icloud.com>
2021-03-02 00:58:07 -0300

    poc development

---
+++ src/graphql/video/Video.ts
@@ -0,0 +1,15 @@
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+class Video {
+  @Field()
+  name: string;
+  @Field()
+  description: string;
+  @Field()
+  _id: string;
+  @Field()
+  category: string;
+}
+
+export default Video
